5f49eca078f67883ea2cc271df0faaed8bfc305d
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-10 22:50:21 +0400

    Setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Router";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
