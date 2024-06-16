dee7e11f8c730901f136c15db400205e34752e9e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-17 03:05:19 +0400

    Update Refresh Token

--- Frontend/src/Utils/Links.ts
+++ Frontend/src/Utils/Links.ts
@@ -1,90 +1,95 @@
 import { REFRESH_TOKENS_MUTATION } from "@/GraphQL/Mutation/Auth.Mutation";
-import { ApolloLink, HttpLink, Observable } from "@apollo/client";
+import { ApolloLink, HttpLink, Observable, Operation } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { jwtDecode } from "jwt-decode";
 import { client } from "./ApolloProvider";
 import AuthStore from "@/Stores/Auth.Store";
+import { toast } from "react-toastify";
+
+//! TODO: FIXME: In Production, change the uri to the deployed server and save it in .env file
+const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+
+const getTokens = () => {
+  const accessToken = AuthStore.getState().accessToken ?? "";
+  const refreshToken = AuthStore.getState().refreshToken ?? "";
+
+  return { accessToken, refreshToken };
+};
 
 const authLink = setContext((__, { headers }) => {
-  const accessToken = localStorage.getItem("accessToken");
+  const { accessToken } = getTokens();
   return {
     headers: {
       ...headers,
-      authorization: accessToken ? `Bearer ${accessToken}` : "",
+      authorization: `Bearer ${accessToken}`,
     },
   };
 });
 
+const requiresAuth = (operation: Operation) => {
+  return operation.getContext().requiresAuth;
+};
+
 const refreshLink = new ApolloLink((operation, forward) => {
   return new Observable((observer) => {
     let handle: any;
-    Promise.resolve(operation)
-      .then((operation) => {
-        const { requiresAuth } = operation.getContext();
 
-        if (!requiresAuth) {
-          return forward(operation);
-        }
-        const accessToken = localStorage.getItem("accessToken");
-        const refreshToken = localStorage.getItem("refreshToken");
+    const { refreshToken } = getTokens();
+    const isRetry = operation.getContext().__retry;
 
-        if (!accessToken || !refreshToken) {
-          window.location.href = "/login";
-          return;
-        }
+    if (!requiresAuth(operation) || isRetry) {
+      return forward(operation).subscribe(observer);
+    }
 
-        const decodedToken: any = jwtDecode(accessToken);
-        const isTokenExpired = decodedToken.exp * 1000 < Date.now();
+    const handleResponse = (response: any) => {
+      const { errors } = response;
+      if (errors && response.errors.some((err: any) => err.message === "Unauthorized")) {
+        client
+          .mutate({
+            mutation: REFRESH_TOKENS_MUTATION,
+            variables: { refreshToken },
+          })
+          .then(({ data }) => {
+            const { accessToken, refreshToken } = data.refreshTokens;
+            AuthStore.getState().setTokens({ accessToken, refreshToken });
 
-        if (isTokenExpired) {
-          // Ensure client is defined before using it
-          return client
-            .mutate({
-              mutation: REFRESH_TOKENS_MUTATION,
-              variables: { refreshToken },
-            })
-            .then(({ data }) => {
-              const { refreshTokens } = data;
-              AuthStore.getState().setTokens(refreshTokens);
-              operation.setContext(({ headers = {} }) => ({
-                headers: {
-                  ...headers,
-                  authorization: `Bearer ${refreshTokens.accessToken}`,
-                },
-              }));
-
-              return forward(operation);
-            })
-            .catch(() => {
-              AuthStore.getState().clearTokens();
-              throw new Error("Token refresh failed");
+            operation.setContext({
+              headers: {
+                ...operation.getContext().headers,
+                authorization: `Bearer ${accessToken}`,
+              },
+              __retry: true,
             });
-        } else {
-          operation.setContext(({ headers = {} }) => ({
-            headers: {
-              ...headers,
-              authorization: `Bearer ${accessToken}`,
-            },
-          }));
-          return forward(operation);
-        }
-      })
-      .then((observable) => {
-        handle = observable?.subscribe({
-          next: observer.next.bind(observer),
-          error: observer.error.bind(observer),
-          complete: observer.complete.bind(observer),
-        });
-      })
-      .catch(observer.error.bind(observer));
+
+            const subscriber = forward(operation).subscribe(
+              handleResponse,
+              observer.error.bind(observer),
+              observer.complete.bind(observer)
+            );
+            if (subscriber) handle.unsubscribe();
+          })
+          .catch((refreshError) => {
+            AuthStore.getState().clearTokens();
+            window.location.href = "/login";
+            observer.error(refreshError);
+          });
+      } else {
+        observer.next(response);
+        observer.complete();
+      }
+    };
+
+    const handleError = (error: any) => {
+      toast.error(error.message);
+      AuthStore.getState().clearTokens();
+      window.location.href = "/login";
+    };
+
+    handle = forward(operation).subscribe(handleResponse, handleError);
 
     return () => {
       if (handle) handle.unsubscribe();
     };
   });
 });
-
-//! TODO: FIXME: In Production, change the uri to the deployed server and save it in .env file
-const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
 
 export const link = ApolloLink.from([refreshLink, authLink, httpLink]);
