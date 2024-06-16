fd0008ac2a6bc1f200658838eeee7d41d672a9ad
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-16 11:54:46 +0400

    Login Page

--- Frontend/src/Pages/Auth/Login.tsx
+++ Frontend/src/Pages/Auth/Login.tsx
@@ -1,44 +1,76 @@
+import Button from "@/Components/UI/Button/Button";
+import Input from "@/Components/UI/Input/Input";
 import { LOGIN_MUTATION } from "@/GraphQL/Mutation/Auth.Mutation";
+import { useInput } from "@/Hooks/UseInput";
+import AuthStore from "@/Stores/Auth.Store";
 import { useMutation } from "@apollo/client";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [login] = useMutation(LOGIN_MUTATION);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+  const { setTokens } = AuthStore();
+
+  const usernameInput = useInput((username) => username.length > 5);
+  const passwordInput = useInput((password) => password.length > 5);
+
+  const errors = [
+    usernameInput.hasError,
+    !usernameInput.value,
+    passwordInput.hasError,
+    !passwordInput.value,
+  ];
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      const response = await login({ variables: { username, password } });
-      if (response.data.login) {
-        localStorage.setItem("accessToken", response.data.login.accessToken);
-        localStorage.setItem("refreshToken", response.data.login.refreshToken);
-        navigate("/profile");
-      }
-    } catch (error) {
-      console.error(error);
-      // Handle login errors
+    if (errors.some((error) => error)) {
+      toast.error("Please fill all the fields");
+      return;
     }
+    await login({
+      variables: { username: usernameInput.value, password: passwordInput.value },
+    })
+      .then(({ data }) => {
+        setTokens(data.login);
+        toast.success("Login successful");
+      })
+      .catch(() => {
+        toast.error("Invalid credentials");
+      });
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
+    <div className="flex flex-col justify-center items-center h-full text-primary">
+      <form
+        onSubmit={handleSubmit}
+        className="px-4 py-5 my-2 shadow-lg bg-slate-100 rounded-xl flex flex-col gap-5"
+      >
+        <h1 className="text-2xl font-bold">Login</h1>
+        <Input
+          id="username"
           type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          autoComplete="username"
+          label="username"
+          errorMessage="Invalid Username"
+          {...usernameInput}
         />
-        <input
+        <Input
+          id="password"
           type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          label="Password"
+          autoComplete="current-password"
+          errorMessage="Invalid Password"
+          {...passwordInput}
         />
-        <button type="submit">Login</button>
+        <Button type="submit">Continue</Button>
       </form>
+      <div className="flex flex-col justify-center items-center gap-2 mt-2">
+        <p className="border-b border-primary pb-1 px-5">New to Regnes?</p>
+        <Link to="/register">
+          <Button className="rounded-full shadow-inner border-b py-1" btnType="secondary">
+            Create your Account
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
